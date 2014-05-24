7fca5f6c2ba674c1276a8e5688355840e79d2e2e
Michael Brennock <mbrennock@gmail.com>
2014-05-23 20:47:07 -0700

    Updated: File Loading, Saving, and Deleting are working on a basic level.

--- gen/com/example/fileassignment_megabyte/R.java
+++ gen/com/example/fileassignment_megabyte/R.java
@@ -24,12 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int EditText1=0x7f080004;
-        public static final int action_settings=0x7f080005;
-        public static final int buttonAppendText=0x7f080002;
-        public static final int buttonReadText=0x7f080003;
-        public static final int buttonReplaceText=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int buttonDelete=0x7f080005;
+        public static final int buttonLoad=0x7f080004;
+        public static final int buttonSave=0x7f080002;
+        public static final int etInput=0x7f080001;
+        public static final int tvInstructions=0x7f080000;
+        public static final int tvOutput=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,11 +39,11 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int AppendButtonText=0x7f050005;
+        public static final int DeleteButtonText=0x7f050006;
         public static final int EditText=0x7f050002;
-        public static final int ReadButtonText=0x7f050004;
-        public static final int ReplaceButtonText=0x7f050006;
-        public static final int TextViewInstructions=0x7f050003;
+        public static final int InstructionsText=0x7f050003;
+        public static final int LoadButtonText=0x7f050004;
+        public static final int SaveButtonText=0x7f050005;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
     }
--- src/com/example/fileassignment_megabyte/MainActivity.java
+++ src/com/example/fileassignment_megabyte/MainActivity.java
@@ -9,30 +9,65 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	String fileName = "myFile.txt";
+	String fileData = "";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void buttonLoad_pressed(View view){
+		makeToast("Loading...");
+		
+		TextView tvOutput = (TextView) findViewById(R.id.tvOutput);
+		fileData = loadFile(fileName);
+		tvOutput.setText(fileData);
+
+		EditText etInput = (EditText) findViewById(R.id.etInput);
+
+		//Clear input for the next round		
+		etInput.setText("");
 	}
 	
-	//btnReadText: WIP
+
+	public void buttonSave_pressed(View view){
+		makeToast("Saving...");
+		
+		EditText etInput = (EditText) findViewById(R.id.etInput);
+		TextView tvOutput = (TextView) findViewById(R.id.tvOutput);
+		String output = tvOutput.getText() + " " + etInput.getText();
+		
+		saveFile(fileName, output);
+
+		tvOutput.setText(output);
+
+		//Clear input for the next round
+		etInput.setText("");
+	}
 	
 	
-	//btnAppendText: WIP
+	public void buttonDelete_pressed(View view){
+		makeToast("Deleting...");
+
+		//It's kinda crude I'll admit, but it gets the job done
+		saveFile(fileName, " ");
+
+
+		EditText etInput = (EditText) findViewById(R.id.etInput);
+
+		//Clear input for the next round
+		etInput.setText("");		
+	}
 	
-	
-	//btnReplaceText: WIP
 	
 	
 	//File I/O examples from the following website:
@@ -43,12 +78,13 @@ public class MainActivity extends Activity {
 		{
 			FileOutputStream fos = openFileOutput(filename, Context.MODE_PRIVATE);
 			fos.write(data.getBytes());
+			
 			fos.close();
-			toast("File successfully saved.");
+			makeToast("File successfully saved.");
 		}
 		catch (Exception e)
 		{
-			toast("Error saving file: " + e.getLocalizedMessage());
+			makeToast("Error saving file: " + e.getLocalizedMessage());
 		}
 	}
 
@@ -68,23 +104,23 @@ public class MainActivity extends Activity {
 				
 			reader.close();
 			fis.close();
-			toast("File successfully loaded.");
+			makeToast("File successfully loaded.");
 			return input;
 				
 		}
 		catch (Exception e)
 		{
-			toast("Error loading file: " + e.getLocalizedMessage());
+			makeToast("Error loading file: " + e.getLocalizedMessage());
 			return "";
 		}
 	}
 	
 	//File I/O examples from the following website:
 	//http://people.bridgewater.edu/~arn002/csci440/android-tutorial.htm#file
-	private void toast(String text)
+	private void makeToast(String text)
 	{
-		Context context = getApplicationContext();
-		Toast toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
+		Context context = this.getApplicationContext();
+		Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
 		toast.show();
 	}
 }
